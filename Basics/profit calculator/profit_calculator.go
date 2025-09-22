80c8621800be4b53f6b470b09b5280a1fc22f765
ajay dev <devadigaajay1729@gmail.com>
2025-09-23 00:43:55 +0530

    profit calculator

---
+++ Basics/profit calculator/profit_calculator.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var revenue float64
+	var expenses float64
+	var taxRate float64
+
+	fmt.Println("Enter revenue:")
+	fmt.Scan(&revenue)
+
+	fmt.Println("Enter expenses:")
+	fmt.Scan(&expenses)
+
+	fmt.Println("Enter tax rate:")
+	fmt.Scan(&taxRate)
+
+	ebt := revenue - expenses
+	profit := ebt * (1 + taxRate/100)
+	ratio := ebt / profit
+
+	fmt.Println("EBT:", ebt)
+	fmt.Println("Profit:", profit)
+	fmt.Println("Ratio:", ratio)
+}
