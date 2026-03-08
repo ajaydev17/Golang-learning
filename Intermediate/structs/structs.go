072a4b82ae40dd1a39c9bac11366b0bd1afe333c
ajay dev <devadigaajay1729@gmail.com>
2026-03-08 12:34:38 +0530

    structs with pointers

---
+++ Intermediate/structs/structs.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type User struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
+
+func main() {
+	userFirstName := getUserData("Enter your first name: ")
+	userLastName := getUserData("Enter your last name: ")
+	userBirthDate := getUserData("Enter your birth date (YYYY-MM-DD): ")
+
+	var appUser User
+
+	appUser = User{
+		firstName: userFirstName,
+		lastName:  userLastName,
+		birthDate: userBirthDate,
+		createdAt: time.Now(),
+	}
+
+	outputUserData(&appUser)
+}
+
+func outputUserData(user *User) {
+	fmt.Println(user.firstName, user.lastName, user.birthDate)
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var userInput string
+	fmt.Scanln(&userInput)
+	return userInput
+}
