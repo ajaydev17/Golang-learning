fcfead319281e506e2969acb9a5c38838957ed8c
ajay dev <devadigaajay1729@gmail.com>
2026-05-18 02:57:04 +0530

    database setup with sqlite3

--- Expert/REST API/models/event.go
+++ Expert/REST API/models/event.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"example.com/rest-api/db"
+)
 
 type Event struct {
 	ID          int
@@ -13,8 +17,27 @@ type Event struct {
 
 var events []Event = []Event{}
 
-func (e Event) Save() {
-	events = append(events, e)
+func (e *Event) Save() error {
+	query := `INSERT INTO events (name, description, location, date_time, user_id) VALUES (?, ?, ?, ?, ?)`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		panic("Failed to prepare statement: " + err.Error())
+	}
+
+	defer stmt.Close()
+
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		panic("Failed to execute statement: " + err.Error())
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		panic("Failed to retrieve last insert ID: " + err.Error())
+	}
+
+	e.ID = int(id)
+	return err
 }
 
 func GetAllEvents() []Event {
