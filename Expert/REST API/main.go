ea90b08126ea7e13e7b5ca17e23ec425953ffa5d
ajay dev <devadigaajay1729@gmail.com>
2026-05-18 02:25:32 +0530

    initial rest-api set up with gin

---
+++ Expert/REST API/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+
+	"example.com/rest-api/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	server.Run(":8080")
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	if err := context.ShouldBindJSON(&event); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Could not parse event data" + err.Error()})
+		return
+	}
+	event.ID = 1
+	event.UserID = 1
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created successfully", "event": event})
+}
