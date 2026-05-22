51165cbd36cd088d0f7124f76b4d67bdf822a76f
ajay dev <devadigaajay1729@gmail.com>
2026-05-23 01:13:36 +0530

    refactoring and outsourcing routes

---
+++ Expert/REST API/routes/event.go
@@ -0,0 +1,51 @@
+package routes
+
+import (
+	"example.com/rest-api/models"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getEvents(context *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve events"})
+		return
+	}
+	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+		return
+	}
+
+	event, err := models.GetEventByID(id)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve event"})
+		return
+	}
+	context.JSON(http.StatusOK, event)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	if err := context.ShouldBindJSON(&event); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Could not parse event data" + err.Error()})
+		return
+	}
+	event.ID = 1
+	event.UserID = 1
+
+	err := event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create event"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created successfully", "event": event})
+}
